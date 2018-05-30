ec68a67032cf4174f37e7d18d4dbb1a74e2da90c
Unknown <orson.hoeksema@pcc.edu>
2018-05-29 22:27:56 -0700

    Update batch script to work in my environment

--- Release/Pushit.bat
+++ Release/Pushit.bat
@@ -1,1 +1,14 @@
-git push azure master 
+@rem This script pushes master branch to our website
+@rem
+@rem OrsonH
+@rem __________________________________________________________________________
+
+@echo off
+if not "%Verbose%"=="" echo on
+@setlocal
+
+set gitdir=c:\tools\git
+set path=%gitdir%\cmd;%path%
+git push azure master
+
+@endlocal
