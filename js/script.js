05351ff44c97fae3c4d034a2bf05b165aabb3a95
Jaredm95 <38049917+Jaredm95@users.noreply.github.com>
2018-06-05 14:20:16 -0700

    Search Function
    Finished the Search Function

--- js/script.js
+++ js/script.js
@@ -20,18 +20,87 @@ function searching(value, type) {
     console.log(Object);
 
     let i;
+    let url;
     for (i = 0; i < Object.results.length; i++) {
         if (Object.results[i].name == value) {
-            window.location = type + ".html?=" + (counter + 1);
+            url = Object.results[i].url;
+            break;
         }
         counter++;
     }
-    if (i == Object.results.length) {
-        let xhr = new XMLHttpRequest(); // XML Request
-        xhr.open("GET", "", false); // Requesting all the pokemon in the database
-        xhr.send(); // sending the request
-        console.log(xhr.status); // Logging the status of the request
-        Object = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
-        console.log(Object);
+    let c;
+    let d;
+    if (i < Object.results.length) {
+        for (let x = 0; x < url.length; x++) {
+            c = url.charAt(x);
+            if (!isNaN(parseInt(c, 10))) {
+                d = url.charAt(x + 1);
+                if (!isNaN(parseInt(d, 10))) {
+                    break;
+                } else if (d == "/") {
+                    d = " ";
+                    break;
+                }
+            }
+        }
+        if (d == " ") {
+            window.location = type + "html?=" + c;
+        } else {
+            window.location = type + "html?=" + c + d;
+        }
+    } else {
+        if (i == Object.results.length && Object.next != null) {
+            callingAPI(counter, value, type, Object.next);
+        } else {
+            console.log("Should be seeing this on bad searches");
+        }
+    }
+}
+// Function for calling the API multiple times for searches
+function callingAPI(counter, value, type, search) {
+    console.log(counter);
+    let xhr = new XMLHttpRequest(); // XML Request
+    console.log(search);
+    xhr.open("GET", search, false); // Requesting all the pokemon in the database
+    xhr.send(); // sending the request
+    console.log(xhr.status); // Logging the status of the request
+    Object = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
+    console.log(Object);
+
+    let i;
+    let url;
+    for (i = 0; i < Object.results.length; i++) {
+        if (Object.results[i].name == value) {
+            url = Object.results[i].url;
+            break;
+        }
+        counter++;
+    }
+    let c;
+    let d;
+    if (i < Object.results.length) {
+        for (let x = 0; x < url.length; x++) {
+            c = url.charAt(x);
+            if (!isNaN(parseInt(c, 10))) {
+                d = url.charAt(x + 1);
+                if (!isNaN(parseInt(d, 10))) {
+                    break;
+                } else if (d == "/") {
+                    d = " ";
+                    break;
+                }
+            }
+        }
+        if (d == " ") {
+            window.location = type + ".html?=" + c;
+        } else {
+            window.location = type + ".html?=" + c + d;
+        }
+    } else {
+        if (i == Object.results.length && Object.next != null) {
+            callingAPI(counter, value, type, Object.next);
+        } else {
+            console.log("Should be seeing this on bad searches");
+        }
     }
 }
