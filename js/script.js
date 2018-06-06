ad986616c4ed900339b68ba990e77f0c18f89b23
Jaredm95 <38049917+Jaredm95@users.noreply.github.com>
2018-06-06 15:34:44 -0700

    Improved Suggestion List
    Improved the Suggestion list

--- js/script.js
+++ js/script.js
@@ -55,18 +55,18 @@ function callingAPI(value, type, search) {
     xhr.open("GET", search, false); // Requesting all the pokemon in the database
     xhr.send(); // sending the request
     console.log(xhr.status); // Logging the status of the request
-    Object = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
+    firstObject = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
     let i;
     let url;
-    for (i = 0; i < Object.results.length; i++) {
-        if (Object.results[i].name == value) {
+    for (i = 0; i < firstObject.results.length; i++) {
+        if (firstObject.results[i].name == value) {
             url = Object.results[i].url;
             break;
         }
     }
     let c;
     let d;
-    if (i < Object.results.length) {
+    if (i < firstObject.results.length) {
         for (let x = 0; x < url.length; x++) {
             c = url.charAt(x);
             if (!isNaN(parseInt(c, 10))) {
@@ -85,8 +85,8 @@ function callingAPI(value, type, search) {
             window.location = type + ".html?=" + c + d;
         }
     } else {
-        if (i == Object.results.length && Object.next != null) {
-            callingAPI(value, type, Object.next);
+        if (i == firstObject.results.length && Object.next != null) {
+            callingAPI(value, type, firstObject.next);
         } else {
             badSearch(value, type);
         }
@@ -103,80 +103,73 @@ function badSearch(value, type) {
     eMessage.style.display = "block";
     eMessage.innerHTML = "Whoops! Can't seem to find that " + "<br>" + "Did you mean: ";
     document.getElementsByClassName("well")[0].style.display = "block";
-    let Object = badCall("https://swapi.co/api/" + type + "/");
-    for (let i = 0; i < Object.results.length; i++) {
-        if (firstLetter == Object.results[i].name.charAt(0)) {
-            let listItem = document.createElement("li");
-            let a = document.createElement("a");
-            a.innerHTML = Object.results[i].name;
-            let url = foundSomething(Object.results[i], type);
-            a.setAttribute("href", url);
-            listItem.appendChild(a);
-            area.appendChild(listItem);
-        }
-    }
-    let Object2;
-    if (Object.hasOwnProperty("next")) {
-        Object2 = badCall(Object.next);
-        for (let i = 0; i < Object2.results.length; i++) {
-            if (firstLetter == Object2.results[i].name.charAt(0)) {
+
+    let firstObject = badCall("https://swapi.co/api/" + type + "/");
+
+    if (type == "films") {
+        for (let i = 0; i < firstObject.results.length; i++) {
+            if (firstLetter == firstObject.results[i].title.charAt(0)) {
                 let listItem = document.createElement("li");
                 let a = document.createElement("a");
-                a.innerHTML = Object2.results[i].name;
-                let url = foundSomething(Object2.results[i], type);
+                a.innerHTML = firstObject.results[i].title;
+                let url = foundSomething(firstObject.results[i], type);
                 a.setAttribute("href", url);
                 listItem.appendChild(a);
                 area.appendChild(listItem);
             }
         }
-    }
-    if (Object2.hasOwnProperty("next")) {
-        let Object3 = badCall(Object2.next);
-        for (let i = 0; i < Object3.results.length; i++) {
-            if (firstLetter == Object3.results[i].name.charAt(0)) {
-                let listItem = document.createElement("li");
-                let a = document.createElement("a");
-                a.innerHTML = Object3.results[i].name;
-                let url = foundSomething(Object3.results[i], type);
-                a.setAttribute("href", url);
-                listItem.appendChild(a);
-                area.appendChild(listItem);
+    } else {
+        while (true) {
+            for (let i = 0; i < firstObject.results.length; i++) {
+                if (firstLetter == firstObject.results[i].name.charAt(0)) {
+                    let listItem = document.createElement("li");
+                    let a = document.createElement("a");
+                    a.innerHTML = firstObject.results[i].name;
+                    let url = foundSomething(firstObject.results[i], type);
+                    a.setAttribute("href", url);
+                    listItem.appendChild(a);
+                    area.appendChild(listItem);
+                }
             }
-        }
-    }
-}
-// Function for calling the API 
-function badCall(search) {
-    let xhr = new XMLHttpRequest(); // XML Request
-    xhr.open("GET", search, false); // Requesting all the pokemon in the database
-    xhr.send(); // sending the request
-    Object = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
-    return Object;
-}
-// Function for clearing the Error Message and Suggestion List
-function clearing() {
-    document.getElementById("errorMess").style.display = "none";
-    document.getElementById("suggList").style.display = "none";
-    document.getElementById("suggList").innerHTML = " ";
-    document.getElementsByClassName("well")[0].style.display = "none";
-}
-// Function for redirecting the suggested item, kill me please
-function foundSomething(results, type) {
-    let c;
-    let d;
-    url = results.url;
-    for (let x = 0; x < url.length; x++) {
-        c = url.charAt(x);
-        if (!isNaN(parseInt(c, 10))) {
-            d = url.charAt(x + 1);
-            if (!isNaN(parseInt(d, 10))) {
-                break;
-            } else if (d == "/") {
-                d = " ";
+            firstObject = badCall(firstObject.next);
+            if(firstObject.next == null){
                 break;
             }
         }
     }
-    let message = type + ".html?=" + c + d
-    return message;
 }
+    // Function for calling the API 
+    function badCall(search) {
+        let xhr = new XMLHttpRequest(); // XML Request
+        xhr.open("GET", search, false); // Requesting all the pokemon in the database
+        xhr.send(); // sending the request
+        firstObject = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
+        return firstObject;
+    }
+    // Function for clearing the Error Message and Suggestion List
+    function clearing() {
+        document.getElementById("errorMess").style.display = "none";
+        document.getElementById("suggList").style.display = "none";
+        document.getElementById("suggList").innerHTML = " ";
+        document.getElementsByClassName("well")[0].style.display = "none";
+    }
+    // Function for redirecting the suggested item, kill me please
+    function foundSomething(results, type) {
+        let c;
+        let d;
+        url = results.url;
+        for (let x = 0; x < url.length; x++) {
+            c = url.charAt(x);
+            if (!isNaN(parseInt(c, 10))) {
+                d = url.charAt(x + 1);
+                if (!isNaN(parseInt(d, 10))) {
+                    break;
+                } else if (d == "/") {
+                    d = " ";
+                    break;
+                }
+            }
+        }
+        let message = type + ".html?=" + c + d
+        return message;
+    }
