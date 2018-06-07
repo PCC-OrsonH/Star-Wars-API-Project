14d271151883d1e1b51015a7d7aab9a7db8fcef9
Jaredm95 <38049917+Jaredm95@users.noreply.github.com>
2018-06-06 19:42:46 -0700

    Refactoring and comments
    Cleaned up a bit and added comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,33 @@
 // Function for getting the user search
 function getSearch() {
+    /* Variables */
+
     let value = document.getElementById("search").value;
 
     let type = document.getElementsByTagName("select")[0].value;
+
+    /* Calling the searching Function */
 
     searching(value, type);
 }
 // Function for validating the search
 function searching(value, type) {
-    let Object;
-    let xhr = new XMLHttpRequest(); // XML Request
-    xhr.open("GET", "https://swapi.co/api/" + type + "/", false); // Requesting all the pokemon in the database
-    xhr.send(); // sending the request
-    firstObject = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
-    let i;
+    /* Variables */
+
+    let firstObject;
+    let i; 
     let url;
+    let c;
+    let d;
+
+    /* XML Request */
+    let xhr = new XMLHttpRequest(); 
+    xhr.open("GET", "https://swapi.co/api/" + type + "/", false);
+    xhr.send();
+    firstObject = JSON.parse(xhr.responseText);
+ 
+    /* Searching for value */
+
     value = value.charAt(0).toUpperCase() + value.slice(1);
     for (i = 0; i < firstObject.results.length; i++) {
         if (firstObject.results[i].name == value) {
@@ -22,8 +35,9 @@ function searching(value, type) {
             break;
         }
     }
-    let c;
-    let d;
+ 
+    /* Searching for ID */ 
+
     if (i < firstObject.results.length) {
         for (let x = 0; x < url.length; x++) {
             c = url.charAt(x);
@@ -52,21 +66,33 @@ function searching(value, type) {
 }
 // Function for calling the API multiple times for searches
 function callingAPI(value, type, search) {
-    let xhr = new XMLHttpRequest(); // XML Request
-    xhr.open("GET", search, false); // Requesting all the pokemon in the database
-    xhr.send(); // sending the request
-    console.log(xhr.status); // Logging the status of the request
-    firstObject = JSON.parse(xhr.responseText); // Declaring a variable for the JSON object
+    /* Variables */
+
+    let firstObject;
     let i;
     let url;
+    let c;
+    let d;
+
+    /* XMl Request */
+
+    let xhr = new XMLHttpRequest(); 
+    xhr.open("GET", search, false); 
+    xhr.send(); 
+    console.log(xhr.status); 
+    firstObject = JSON.parse(xhr.responseText); 
+
+    /* Searching for value */ 
+
     for (i = 0; i < firstObject.results.length; i++) {
         if (firstObject.results[i].name == value) {
             url = firstObject.results[i].url;
             break;
         }
     }
-    let c;
-    let d;
+
+    /* Searching for ID */
+
     if (i < firstObject.results.length) {
         for (let x = 0; x < url.length; x++) {
             c = url.charAt(x);
@@ -87,7 +113,7 @@ function callingAPI(value, type, search) {
         }
     } else {
         if (i == firstObject.results.length && firstObject.next != null) {
-            callingAPI(value, type, firstObject.next);
+            callingAPI(value, type, firstObject.next); // Recalling the function with the new API URL
         } else {
             badSearch(value, type);
         }
@@ -95,17 +121,24 @@ function callingAPI(value, type, search) {
 }
 // Function for making suggestions on bad searches
 function badSearch(value, type) {
-    let firstLetter = value.charAt(0);
+    /* Variables */
 
+    let firstLetter = value.charAt(0);
     let area = document.getElementById("suggList");
+    let eMessage = document.getElementById("errorMess");
+
+    /* Clearing and displaying sections */
+
     area.innerHTML = " ";
     area.style.display = "block";
-    let eMessage = document.getElementById("errorMess");
     eMessage.style.display = "block";
     eMessage.innerHTML = "Whoops! Can't seem to find that " + "<br>" + "Did you mean: ";
     document.getElementsByClassName("well")[0].style.display = "block";
 
+    // Getting the first list in the category 
     let firstObject = badCall("https://swapi.co/api/" + type + "/");
+
+    /* Searching the List for suggestions */
 
     if (type == "films") {
         for (let i = 0; i < firstObject.results.length; i++) {
@@ -152,6 +185,11 @@ function badSearch(value, type) {
 }
 // Function for calling the API 
 function badCall(search) {
+    /* Variables */
+    let firstObject;
+
+    /* XML Request */
+
     let xhr = new XMLHttpRequest(); // XML Request
     xhr.open("GET", search, false); // Requesting all the pokemon in the database
     xhr.send(); // sending the request
@@ -160,6 +198,8 @@ function badCall(search) {
 }
 // Function for clearing the Error Message and Suggestion List
 function clearing() {
+    /* Clearing lists */
+
     document.getElementById("errorMess").style.display = "none";
     document.getElementById("suggList").style.display = "none";
     document.getElementById("suggList").innerHTML = " ";
@@ -167,8 +207,14 @@ function clearing() {
 }
 // Function for redirecting the suggested item, kill me please
 function foundSomething(results, type) {
+    /* Variables */
+
+    let message;
+    let url;
     let c;
     let d;
+    
+    /* Searching for ID */
     url = results.url;
     for (let x = 0; x < url.length; x++) {
         c = url.charAt(x);
@@ -182,6 +228,6 @@ function foundSomething(results, type) {
             }
         }
     }
-    let message = type + ".html?=" + c + d
+    message = type + ".html?=" + c + d
     return message;
 }
