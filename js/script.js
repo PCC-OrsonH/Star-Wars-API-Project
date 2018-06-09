09be86a6c8d8bcb6d51e3aaf780d99ba6b469108
Jaredm95 <38049917+Jaredm95@users.noreply.github.com>
2018-06-09 16:14:06 -0700

    Switched searching
    Made some minor changes that should have sped up the searching even more

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-// Class for storing the searches, this API seriously so bad i have to use OOP to make it faster : )
-class Result {
-    /* Constructor */
-    constructor(stuff) {
-        this.thing = stuff;
-    }
+// Function for getting the user search
+function getSearch() {
+    /* Variables */
+    let type = document.getElementsByTagName("select")[0].value;
+    let value = document.getElementById("search").value;
 
-    /* Methods */
-    getStuff() {
-        return this.thing;
-    }
+    /* Calling the Function that stores the API calls */
+    thisAPISux(type, value);
 }
 // Function for calling the API and storing the results
-function thisAPISux(type) {
+function thisAPISux(type, value) {
     /* Variables */
     let firstObject;
     let thing = [];
@@ -22,16 +19,11 @@ function thisAPISux(type) {
 
     /* Storing the rest of the objects */
     function checking(foundObject) {
-        if (foundObject.next == null) {
-            thing[x] = new Result(foundObject);
-            searching(thing, type); // Searching for the search value
-        } else {
-            thing[x] = new Result(foundObject);
-            x++;
-            callingAPI(foundObject.next);
-        }
+        thing[x] = foundObject;
+        searching(thing[x])
     }
-    // Function for calling the API 
+    
+    /* Function for calling the API */
     function callingAPI(search) {
         /* Variables */
         let foundObject
@@ -47,68 +39,57 @@ function thisAPISux(type) {
             }
         }
     }
-}
-// Function for getting the user search
-function getSearch() {
-    /* Variables */
-    let type = document.getElementsByTagName("select")[0].value;
+    // Function for validating the search
+    function searching(stuff) {
+        /* Variables */
+        let i;
+        let url = null;
+        let c;
+        let d;
 
-    /* Calling the Function that stores the API calls */
-    thisAPISux(type);
-}
-// Function for validating the search
-function searching(thing, type) {
-    /* Variables */
-    let i;
-    let x;
-    let url = null;
-    let c;
-    let d;
-    let value;
-
-    /* Searching for value */
-    value = document.getElementById("search").value;
-    value = value.charAt(0).toUpperCase() + value.slice(1);
-    for (i = 0; i < thing.length; i++) {
-        for (x = 0; x < thing[i].thing.results.length; x++) {
+        /* Searching for value */
+        value = value.charAt(0).toUpperCase() + value.slice(1);
+        for (i = 0; i < stuff.results.length; i++) {
             if (type == "films") {
-                if (value == thing[i].thing.results[x].title) {
-                    url = thing[i].thing.results[x].url;
+                if (value == stuff.results[i].title) {
+                    url = stuff.results[i].url;
                     break;
                 }
             } else {
-                if (value == thing[i].thing.results[x].name) {
-                    url = thing[i].thing.results[x].url;
+                if (value == stuff.results[i].name) {
+                    url = stuff.results[i].url;
                     break;
                 }
             }
         }
-        if (x < thing[i].thing.results.length) {
-            break;
-        }
-    }
 
-    /* Searching for ID */
-    if (url != null) {
-        for (let x = 0; x < url.length; x++) {
-            c = url.charAt(x);
-            if (!isNaN(parseInt(c, 10))) {
-                d = url.charAt(x + 1);
-                if (!isNaN(parseInt(d, 10))) {
-                    break;
-                } else if (d == "/") {
-                    d = " ";
-                    break;
+        /* Searching for ID */
+        if (url != null) {
+            for (let y = 0; y < url.length; y++) {
+                c = url.charAt(y);
+                if (!isNaN(parseInt(c, 10))) {
+                    d = url.charAt(y + 1);
+                    if (!isNaN(parseInt(d, 10))) {
+                        break;
+                    } else if (d == "/") {
+                        d = " ";
+                        break;
+                    }
                 }
             }
-        }
-        if (d == " ") {
-            window.location = type + ".html?=" + c;
+            if (d == " ") {
+                window.location = type + ".html?=" + c;
+            } else {
+                window.location = type + ".html?=" + c + d;
+            }
         } else {
-            window.location = type + ".html?=" + c + d;
+            if (stuff.next == null) {
+                badSearch(value, thing, type);
+            } else {
+                x++;
+                callingAPI(stuff.next);
+            }
         }
-    } else {
-        badSearch(value, thing, type);
     }
 }
 // Function for making suggestions on bad searches
@@ -128,29 +109,29 @@ function badSearch(value, thing, type) {
 
     /* Searching for suggestions */
     for (let i = 0; i < thing.length; i++) {
-        for (let x = 0; x < thing[i].thing.results.length; x++) {
+        for (let x = 0; x < thing[i].results.length; x++) {
             if (type == "films") {
-                if (firstLetter == thing[i].thing.results[x].title.charAt(0)) {
+                if (firstLetter == thing[i].results[x].title.charAt(0)) {
                     /* Variables */
                     let listItem = document.createElement("li");
                     let a = document.createElement("a");
-                    let url = foundSomething(thing[i].thing.results[x], type);
+                    let url = foundSomething(thing[i].results[x], type);
 
                     /* Displaying the Suggestion */
-                    a.innerHTML = thing[i].thing.results[x].name || thing[i].thing.results[x].title;
+                    a.innerHTML = thing[i].results[x].title;
                     a.setAttribute("href", url);
                     listItem.appendChild(a);
                     area.appendChild(listItem);
                 }
             } else {
-                if (firstLetter == thing[i].thing.results[x].name.charAt(0)) {
+                if (firstLetter == thing[i].results[x].name.charAt(0)) {
                     /* Variables */
                     let listItem = document.createElement("li");
                     let a = document.createElement("a");
-                    let url = foundSomething(thing[i].thing.results[x], type);
+                    let url = foundSomething(thing[i].results[x], type);
 
                     /* Displaying the Suggestion */
-                    a.innerHTML = thing[i].thing.results[x].name || thing[i].thing.results[x].title;
+                    a.innerHTML = thing[i].results[x].name;
                     a.setAttribute("href", url);
                     listItem.appendChild(a);
                     area.appendChild(listItem);
