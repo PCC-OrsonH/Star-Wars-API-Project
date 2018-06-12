ab530223863d84366cc5081622f5e04fc966e42c
Jaredm95 <38049917+Jaredm95@users.noreply.github.com>
2018-06-12 15:03:31 -0700

    starships js
    finished starships Javascript

--- js/starships.js
+++ js/starships.js
@@ -9,6 +9,7 @@ function getInfo() {
     xhr.onreadystatechange = function () {
         if (this.readyState === this.DONE) {
             let newObject = JSON.parse(xhr.responseText);
+            console.log(newObject);
             updateInfo(newObject);
         }
     }
@@ -16,9 +17,9 @@ function getInfo() {
 
 function updateInfo(data) {
     /* Variables */
-    let name = document.getElementById("#name");
-    let model = document.getElementById("#model");
-    let manufacturer = document.getElementById("#manufacturer");
+    let name = document.getElementById("name");
+    let model = document.getElementById("model");
+    let manufacturer = document.getElementById("manufacturer");
     let cost_in_credits = document.querySelector('#cost_in_credits');
     let length = document.querySelector('#length');
     let max_atmosphering_speed = document.querySelector("#max_atmosphering_speed");
@@ -26,8 +27,8 @@ function updateInfo(data) {
     let passengers = document.querySelector("#passengers");
     let cargo_capacity = document.querySelector("#cargo_capacity");
     let consumables = document.querySelector("#consumables");
-    let hyperdrive_rating = document.querySelector("#hyperdrive_rating");pilots
-    let mglt = document.querySelector("#MGLT");
+    let hyperdrive_rating = document.querySelector("#hyperdrive_rating");
+    let mglt = document.querySelector("#mglt");
     let starship_class = document.querySelector("#starship_class");
     let pilots = document.querySelector("#pilots");
     
@@ -45,12 +46,64 @@ function updateInfo(data) {
     hyperdrive_rating.innerText = `${data.hyperdrive_rating}`;
     mglt.innerText = `${data.mglt}`;
     starship_class.innerText = `${data.starship_class}`;
-    pilots.innerText = `${data.pilots}`;
-    for (let x = 0; x < data.characters.length; x++) {
-        foundCharacter(data.characters[x]);
+    for (let x = 0; x < data.films.length; x++) {
+        foundMovie(data.films[x]);
     }
 }
+function foundMovie(stuff) {
+    /* variables */
+    let newObject;
+    let id;
+    let area = document.getElementById("movies");
 
+    /* XML Call */
+    let xhr = new XMLHttpRequest;
+    xhr.open("GET", stuff, true);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (this.readyState === this.DONE) {
+            newObject = JSON.parse(xhr.responseText);
+            /* Variables */
+            let image = document.createElement("img");
+            let a = document.createElement("a");
+            let div = document.createElement("div");
+            let url;
+
+            /* Setting Attributes */
+            switch(newObject.episode_id){
+                case 1:
+                image.setAttribute("src", "images/phantom-menace.jpg");
+                break;
+                case 2:
+                image.setAttribute("src", "images/attack-of-the-clones.jpg");
+                break;
+                case 3:
+                image.setAttribute("src", "images/revenge-of-the-sith.jpg");
+                break;
+                case 4:
+                image.setAttribute("src", "images/a-new-hope.jpg");
+                break;
+                case 5:
+                image.setAttribute("src", "images/empire-strikes-back.jpg");
+                break; 
+                case 6:
+                image.setAttribute("src", "images/return-of-the-jedi.jpg");
+                break;
+                case 7:
+                image.setAttribute("src", "images/force-awakens-poster.jpg");
+                break;
+            }
+            url = foundSomething(newObject, "films");
+            a.setAttribute("href", url);
+            div.setAttribute("class", "col-xs-12 col-sm-6 col-md-4");
+
+            /* Displaying */
+            a.appendChild(image);
+            div.appendChild(a);
+            area.appendChild(div);
+        }
+    }
+}
 function foundCharacter(stuff) {
     /* variables */
     let newObject;
@@ -77,48 +130,3 @@ function foundCharacter(stuff) {
         }
     }
 }
-/*
-let name = document.querySelector('#name');
-let model = document.querySelector('#model');
-let manufacturer = document.querySelector('#manufacturer');
-let cost_in_credits = document.querySelector('#cost_in_credits');
-let length = document.querySelector('#length');
-let max_atmosphering_speed = document.querySelector('#max_atmosphering_speed');
-let crew = document.querySelector('#crew');
-let passengers = document.querySelector('#passengers');
-let cargo_capacity = document.querySelector('#cargo_capacity');
-let consumables = document.querySelector('#consumables');
-let hyperdrive_rating = document.querySelector('#hyperdrive_rating');
-let MGLT = document.querySelector('#MGLT');
-let starship_class = document.querySelector('#starship_class');
-let pilots = document.querySelector('#pilots');
-let films = document.querySelector('#films');
-
-var number = document.URL.substring(document.URL.indexOf('=') + 1);
-//var number = '14';
-
-function getInfo() {
-    axios.get('https://swapi.co/api/starships/' + number + '/').then(function(response) {
-        updateInfo(response.data);
-    });
-}
-
-function updateInfo(data) {
-    name.innerText = `Name: ${data.name}`;
-    model.innerText = `Model: ${data.model}`;
-    manufacturer.innerText = `Manufacturer: ${data.manufacturer}`;
-    cost_in_credits.innerText = `Cost in Credits: ${data.cost_in_credits}`;
-    length.innerText = `Length: ${data.length}`;
-    max_atmosphering_speed.innerText = `Max Atmosphering Speed: ${data.max_atmosphering_speed}`;
-    crew.innerText = `Crew: ${data.crew}`;
-    passengers.innerText = `Passengers: ${data.passengers}`;
-    cargo_capacity.innerText = `Cargo Capacity: ${data.cargo_capacity}`;
-    consumables.innerText = `Consumables: ${data.consumables}`;
-    hyperdrive_rating.innerText = `Hyperdrive Rating: ${data.hyperdrive_rating}`;
-    mglt.innerText = `MGLT: ${data.mglt}`;
-    starship_class.innerText = `Starship Class: ${data.starship_class}`;
-    pilots.innerText = `Pilots: ${data.pilots}`;
-    films.innerText = `Films: ${data.films}`;
-}
-
-button.addEventListener('click', getInfo);*/
